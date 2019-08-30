e35a5ba4cbaab49873124648ee3ea2e3d31f1878
youngmeezz <kymee0918@gmail.com>
2019-08-30 15:21:04 +0900

    maven project

---
+++ src/main/java/kr/co/itcen/network/echo/EchoClient.java
@@ -0,0 +1,10 @@
+package kr.co.itcen.network.echo;
+
+public class EchoClient {
+	private static String SERVER_IP = "192.168.1.16";
+	private static int SERVER_PORT = 8000;
+	
+	public static void main(String[] args) {
+	}
+}
+
---
+++ src/main/java/kr/co/itcen/network/echo/EchoServer.java
@@ -0,0 +1,9 @@
+package kr.co.itcen.network.echo;
+
+public class EchoServer {
+	private static final int PORT = 8000;
+	
+	public static void main(String[] args) {
+	}
+
+}
